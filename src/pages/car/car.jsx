2220131d2a9b8c5ae53c0e3e1861bd0ef60274f8
David Tache <tached@uninorte.edu.co>
2023-10-30 19:06:02 -0500

    minor changes

--- src/pages/car/car.jsx
+++ src/pages/car/car.jsx
@@ -1,25 +1,26 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import Acarreo from '../../components/acarreo';
 import Fruta from '../../components/fruta';
 
 const Car = () => {
     const [total, setTotal] = useState(0);
 
-    const acarreo = useCallback((totalvalue) => {
+    const acarreo = (totalvalue) => {
         const { price, operation } = totalvalue;
-        setTotal(currentTotal => {
-            return operation ? currentTotal + price : currentTotal - price;
-        });
-        console.log('mostrando desde el padre: ', totalvalue);
-    }, []);
+        setTotal(currentTotal => operation ? currentTotal + price : currentTotal - price);
+    };
+
+    useEffect(() => {
+        console.log('Total actualizado: ', total);
+    }, [total]);
 
     return (
         <>
-            <Fruta name={"Manzana"} price={1000} bus={acarreo} />
-            <Fruta name={"Pera"} price={600} bus={acarreo} />
-            <Fruta name={"Mango"} price={800} bus={acarreo} />
-            <Fruta name={"Guanabana"} price={1200} bus={acarreo} />
-            <Fruta name={"Piña"} price={3000} bus={acarreo} />
+            <Fruta name="Manzana" price={1000} bus={acarreo} />
+            <Fruta name="Pera" price={600} bus={acarreo} />
+            <Fruta name="Mango" price={800} bus={acarreo} />
+            <Fruta name="Guanabana" price={1200} bus={acarreo} />
+            <Fruta name="Piña" price={3000} bus={acarreo} />
             <hr />
             <Acarreo total={total} />
         </>
