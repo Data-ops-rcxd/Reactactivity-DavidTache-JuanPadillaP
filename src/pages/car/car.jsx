6b9df54861f511978e180d4842dde71c0e6207e4
David Alejandro Tache Ardila <tached@uninorte.edu.co>
2023-11-14 18:32:31 -0500

    actualizaciones + api

--- src/pages/car/car.jsx
+++ src/pages/car/car.jsx
@@ -2,6 +2,33 @@ import { useState, useEffect } from 'react';
 import Acarreo from '../../components/acarreo';
 import Fruta from '../../components/fruta';
 
+async function searchfruits() {
+
+    const url = "https://api.predic8.de/shop/v2/products"
+    const peticion = await fetch(url, {
+        method: 'GET',
+    })
+
+    if (!peticion.ok) {
+        console.log('Peticion falló');
+    } else {
+        const res = await peticion.json()
+        const response = res.products
+
+        response.forEach(async ({ self_link }) => {
+            const otrapet = await fetch(`https://api.predic8.de${self_link}`)
+            if (!otrapet.ok) {
+                console.log('Peticion 2 falló');
+            } else {
+                const otrapetres = await otrapet.json()
+                const productname = otrapetres.name
+                const productprice = otrapetres.price
+                console.log(otrapetres);
+            }
+        });
+    } 
+}
+
 const Car = () => {
     const [total, setTotal] = useState(0);
 
@@ -13,6 +40,8 @@ const Car = () => {
     useEffect(() => {
         console.log('Total actualizado: ', total);
     }, [total]);
+
+    console.log(searchfruits())
 
     return (
         <>
